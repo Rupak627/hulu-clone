6fa8c32269ac68ff4505506a7d380af8d0320767
Rupak Chauhan <rupak627@gmail.com>
2022-08-17 19:09:51 +0530

    added scrollbar

--- pages/components/Nav.js
+++ pages/components/Nav.js
@@ -6,7 +6,7 @@ function Nav() {
   const router = useRouter();
   return (
     <nav className="relative">
-      <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide">
+      <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll w-4 scrollbar-hide md:scrollbar-default">
         {Object.entries(requests).map(([key, { title, url }]) => (
           <h2
             key={key}
@@ -17,7 +17,7 @@ function Nav() {
           </h2>
         ))}
       </div>
-      <div className="absolute top-0 right-0 bg bg-gradient-to-l from[#06202a] h-10 w-1/12"/>
+      <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12"/>
     </nav>
   );
 }
