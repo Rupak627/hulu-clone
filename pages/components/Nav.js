cc93f4d334299a19dc02baf6a22640acdffeaaf2
Rupak Chauhan <rupak627@gmail.com>
2022-08-18 17:43:06 +0530

    updated

--- pages/components/Nav.js
+++ pages/components/Nav.js
@@ -6,7 +6,7 @@ function Nav() {
   const router = useRouter();
   return (
     <nav className="relative">
-      <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll w-4 scrollbar-hide md:scrollbar-default">
+      <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide md:scrollbar-default">
         {Object.entries(requests).map(([key, { title, url }]) => (
           <h2
             key={key}
